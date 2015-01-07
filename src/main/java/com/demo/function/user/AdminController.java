3c22a7597e19f9370a7124e5564d351ce6604b26
FireTercel <tdy2837@gmail.com>
2015-01-07 23:29:24 +0800

    change

--- src/main/java/com/demo/function/user/AdminController.java
+++ src/main/java/com/demo/function/user/AdminController.java
@@ -1,5 +1,8 @@
 package com.demo.function.user;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import cn.dreampie.routebind.ControllerKey;
 import cn.dreampie.shiro.core.SubjectKit;
 import cn.dreampie.shiro.hasher.Hasher;
@@ -32,10 +35,10 @@ public class AdminController extends Controller {
 		User user=getModel(User.class);
 		User subUser=SubjectKit.getUser();
 		boolean result=false;
-		
+		String todo=getPara("do");
 		HasherInfo hasher = HasherKit.hash(user.getStr("password"));
 		if(user.getStr("first_name")==null)
-			user.set("first_name", "");
+			user.set("first_name", "三角");
 		
 		user.set("password", hasher.getHashResult())
 		.set("salt", hasher.getSalt())
@@ -43,6 +46,33 @@ public class AdminController extends Controller {
 		.set("providername", subUser.getStr("username"))
 		.set("full_name", user.getStr("last_name") + "." + user.getStr("first_name"));
 		result=user.save();
+		
+		/*if(todo!=null){
+			if(todo.equals("delete")&&(user.getLong("id")!=null)){
+				user.set("deleted_at", new Timestamp(new Date().getTime())).update();
+			}else if(todo.equals("save")){
+				HasherInfo hasher = HasherKit.hash(user.getStr("password"));
+				if(user.getStr("first_name")==null)
+					user.set("first_name", "三角");
+				
+				user.set("password", hasher.getHashResult())
+				.set("salt", hasher.getSalt())
+				.set("hasher", hasher.getHasher().value())
+				.set("providername", subUser.getStr("username"))
+				.set("full_name", user.getStr("last_name") + "." + user.getStr("first_name"));
+				result=user.save();
+			}else if(todo.equals("update")&&user.getLong("id")!=null){
+				user.removeNullValueAttrs();
+				Boolean reuse = getParaToBoolean("reuse");
+				if(reuse!=null&&reuse)
+					user.set("deleted_at", null);
+				if(user.getBoolean("last_name")!=null)
+					user.set("full_name", user.get("last_name")+"."+user.get("first_name"));
+				result=user.update();
+			}
+		}*/
+		
+		
 		
         
 		
