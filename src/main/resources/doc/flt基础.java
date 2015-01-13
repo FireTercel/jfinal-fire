0d966ffa11b1aaf562c5db2eaf1f55b9f6a7dbfd
FireTercel <tdy2837@gmail.com>
2015-01-13 17:26:32 +0800

    add a flt

---
+++ src/main/resources/doc/flt基础.java
@@ -0,0 +1,11 @@
+Freemarker 基本变量类型对照 TemplateModel 子接口
+TemplateHashModel 对应哈希表
+	-> SimpleHash 为实现类
+TemplateSequenceModel 对应序列
+	->
+TemplateNumberModel 对应数字
+	->
+TemplateScalarModel 字符串 （不是TemplateStringModel)
+	-> SimpleScalar 为实现类
+TemplateBooleanModel.TRUE 和 TemplateBooleanModel.FALSE 布尔值
+	->
